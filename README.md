6c3c50f48b452da60276c6478144f28667b94b1b
Jiri Jasonek <jiri.jasonek@principal.cz>
2025-04-05 15:48:28 +0200

    Implement userDetailsService method to add more users

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Udemy Course Spring Security Section3
 ## spring version: 3.4.4
 
+## After implementing the userDetailService method, we are getting following error 
+Given that there is no default password encoder configured, each password must have a password encoding prefix. Please either prefix this password with '{noop}' or set a default password encoder in `DelegatingPasswordEncoder`.
+java.lang.IllegalArgumentException: Given that there is no default password encoder configured, each password must have a password encoding prefix. Please either prefix this password with '{noop}' or set a default password encoder in `DelegatingPasswordEncoder`.
+
+For now we use '{noop}' for not using any password encoder.
--- src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -3,6 +3,10 @@ package com.eazybytes.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -26,5 +30,12 @@ public class ProjectSecurityConfig {
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user").password("12345").authorities("read").build();
+        UserDetails admin = User.withUsername("admin").password("54321").authorities("admin").build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
